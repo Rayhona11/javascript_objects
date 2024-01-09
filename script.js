27f224d2bfd916e02a3f573fff1fb1e52902b400
Rayhona <rayhonamirzoyeva.ru>
2024-01-09 14:48:01 +0500

    added javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+//string
+
+let car ={
+    model:"BMW",
+    color:"black",
+    height:"2.0",
+    weight:"200"
+ }
+
+ console.log(car.model);
+
+ //operators
+
+// let a = 5;
+ //let b = 5;
+
+ //console.log(a == b);
+
+ let c = 7;
+ let d = 6;
+
+ console.log(c == 7);
+
+ let a = 3;
+ let b = -6;
+ 
+ console.log(a == -6);
+
