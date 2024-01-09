a9e94cf81739128947a39323e409e54c10d6eff1
Rayhona <rayhonamirzoyeva.ru>
2024-01-09 15:52:33 +0500

    euechbej

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //string
 
-let car ={
-    model:"BMW",
-    color:"black",
-    height:"2.0",
-    weight:"200"
- }
+// let car ={
+    //model:"BMW",
+    //color:"black",
+    //height:"2.0",
+   // weight:"200"
+ //}
 
- console.log(car.model);
+ //console.log(car.model);
 
  //operators
 
@@ -16,17 +16,52 @@ let car ={
 
  //console.log(a == b);
 
- let c = 7;
- let d = 6;
+ //let c = 7;
+ //let d = 6;
 
- console.log(c == 7);
+ //console.log(c == 7);
 
- let b =6;
+ //let b =6;
 
- console.log(b>=0);
+ //console.log(b>=0);
 
-let a =-6;
+//let a =-6;
 
-console.log(a<=0);
+//console.log(a<=0);
+
+//let v = 8;
+//console.log(v % 2 === 0);
+
+// || yoki 
+
+// && va
+
+let x = 4;
+let y = 3;
+let z = 2;
+
+console.log(x ===4 || y ===5 || z === 1);
+console.log(x ===4 && y ===3 || z ===2);
+
+//let x =11;
+//let  y= 16;
+
+//console.log( y>=0  && x>=0);
+
+//let x =4;
+//let y = 8;
+
+//console.log( x % 2 ===0 && y % 2===0);
+
+let result = 30;
+//?Function
+function sayHello(a,b){
+    let result = a + b * 10;
+
+    return result;
+}
+
+console.log(sayHello(12,34));
+
 
 
